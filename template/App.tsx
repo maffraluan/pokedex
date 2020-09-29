641f04ae993a8d9c9350df309d525fa800fc125b
Luan Maffra <luanmaffra@Luans-MacBook-Pro.local>
2020-09-29 12:33:11 -0300

    Getting started...

---
+++ template/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { StatusBar } from 'react-native'
+import Routes from './src/Routes'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
---
+++ template/src/Routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from './screens'
+
+const RootStack = createStackNavigator();
+
+const Routes: React.FC = () => {
+  return (
+    <RootStack.Navigator>
+        <RootStack.Screen name={"Home"} component={HomeScreen} />
+    </RootStack.Navigator>
+  );
+}
+
+export default Routes;
